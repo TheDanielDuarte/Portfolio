0821d8bb156c4a7f09ada93e812c022c62ac7356
Daniel Duarte <danielduarte2004@gmail.com>
2018-07-15 17:00:29 -0400

    Removed unnecessary depency "AngularFire2"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { AngularFireModule } from 'angularfire2';
 import { AppRoutingModule } from './app-routing.module';
-import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AppComponent } from './app.component';
-import { environment } from '@env/environment';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProjectDetailsComponent } from './components/project-details/project-details.component';
@@ -29,9 +25,6 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFirestoreModule,
-    AngularFireModule.initializeApp(environment.firebaseCredentials, 'portfolio'),
-    AngularFireStorageModule,
     BrowserAnimationsModule,
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseCredentials: {
-    apiKey: 'AIzaSyBp1ZHlVL_Y3KDB_8r9tkyPIQiocR_XlUg',
-    authDomain: 'portfolio-d971b.firebaseapp.com',
-    databaseURL: 'https://portfolio-d971b.firebaseio.com',
-    projectId: 'portfolio-d971b',
-    storageBucket: 'portfolio-d971b.appspot.com',
-    messagingSenderId: '715688496927'
-  },
   contentful: {
     spaceID: 'tda5konfwb73',
     token: 'acf6e7946aa3819cb96028ebcdea83c7b5cb2f435ecfd4db6ecb236d41ca867c'
